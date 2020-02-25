a123bf586eb9a8a89f708f0624c62da4c43680f3
Artromterra <wertuozo@yandex.ru>
2020-02-25 10:06:38 +0700

    deploy heroku

--- mailsend/settings.py
+++ mailsend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mailsender-e2-skillfactory.herokuapp.com']
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mailgun.org'
